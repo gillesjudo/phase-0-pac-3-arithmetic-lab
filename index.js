8a093935f868acda9883d3a745c2283f7a10d601
Gilles Castro <gillescastro1021@gmail.com>
2022-01-17 23:54:54 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a=612, b=601) {
+    return (a + b) ;
+}
+function subtract(a=1,b=32) {
+    return (a - b ) ;
+}
+function multiply(a=10,b=10) {
+  return (a*b) ;
+}
+function divide(a=100,b=10) {
+    return (a / b) ;
+}
+function increment(n=988) {
+    return(++n);
+}
+function decrement(n=993) {
+    return(--n);
+}
+function makeInt(n="2") {
+ return parseInt(n, 10) ;
+}
+function preserveDecimal(n="10.55"){
+    return parseFloat(n);
+}
